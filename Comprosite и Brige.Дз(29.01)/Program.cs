3a38b8a2efb3fde430c56b26d9376a482195737d
alinka0605@icloud.com <alinka0605@icloud.com>
2025-02-09 21:34:47 +0300

    Добавьте файлы проекта.

---
+++ Comprosite и Brige.Дз(29.01)/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brige
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine gasolineEngine = new GasolineEngine();
+            Engine electricEngine = new ElectricEngine();
+
+            Vehicle carWithGasoline = new Car(gasolineEngine);
+            Vehicle truckWithElectric = new Truck(electricEngine);
+            Console.WriteLine(carWithGasoline.Drive());
+            Console.WriteLine(truckWithElectric.Drive());
+        }
+    }
+    public interface Engine
+    {
+        void Start();
+        void Worth();
+
+    }
+    public class GasolineEngine : Engine
+    {
+        public void Start()
+        {
+            Console.WriteLine("Бензиновый двигатель запущен");
+
+        }
+        public void Worth()
+        {
+            Console.WriteLine("Бензиновый двигатель выключен");
+        }
+    }
+
+    public class ElectricEngine : Engine
+    {
+        public void Start()
+        {
+            Console.WriteLine("Электрический двигатель запущен");
+        }
+        public void Worth()
+        {
+            Console.WriteLine("Электрический двигатель выключен");
+        }
+    }
+    public abstract class Vehicle
+    {
+        protected Engine _engine;
+
+        public Vehicle(Engine engine)
+        {
+            _engine = engine;
+        }
+        public abstract string Drive();
+    }
+
+    public class Car : Vehicle
+    {
+        public Car(Engine engine) : base(engine) { }
+
+        public override string Drive()
+        {
+            return $"Автомобиль с {_engine.Start()} едет.";
+        }
+    }
+
+
+    public class Truck : Vehicle
+    {
+        public Truck(Engine engine) : base(engine) { }
+
+        public override string Drive()
+        {
+            return $"Грузовик с {_engine.Start()} перевозит груз.";
+        }
+    }
+}
---
+++ Comprosite/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Folder root = new Folder("Мой Комп");
+        Folder documents = new Folder("Документы");
+        documents.Add(new File("Реферат.docx"));
+        documents.Add(new File("Список.xlsx"));
+
+        Folder music = new Folder("Музыка");
+        music.Add(new File("Песня.mp3"));
+
+        root.Add(documents);
+        root.Add(music);
+        root.Add(new File("README.txt"));
+    }
+}
+public interface IFileSystem
+{
+    string Name { get; }
+    void Show();
+}
+
+public class File : IFileSystem
+{
+    public string Name { get; }
+
+    public File(string name)
+    {
+        Name = name;
+    }
+
+    public void Show(string indent)
+    {
+        Console.WriteLine("\tФайл: " + Name);
+    }
+
+    public void Show()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Folder : IFileSystem
+{
+    public string Name { get; }
+    private List<IFileSystem> _children = new List<IFileSystem>();
+
+    public Folder(string name)
+    {
+        Name = name;
+    }
+
+    public void Add(IFileSystem item)
+    {
+        _children.Add(item);
+    }
+
+    public void Remove(IFileSystem item)
+    {
+        _children.Remove(item);
+    }
+
+    public void Show()
+    {
+        Console.WriteLine("\tПапка: " + Name);
+        foreach (var item in _children)
+        {
+            item.Show(" ");
+        }
+    }
+}
+
